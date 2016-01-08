12fb472141a22e89129a448f7d73cd21b1e92d68
rus100 <rus100@yandex.ru>
2016-01-08 17:16:29 +0500

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gomoku
+{
+    class Class1
+    {
+       static public bool pc;
+       static public bool znak;
+       static public int time;
+       static public int kolvohod;
+       static public string imya;
+       static public bool time1;
+       static public bool kolvohod1;
+    }
+}
